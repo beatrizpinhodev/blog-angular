8ba07ed5a1be4fa162cae447c86689a5cf114143
beatrizpinho <beatrizbrps@gmail.com>
2023-06-04 20:10:04 -0300

    components with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigPhotoComponent } from './components/big-photo/big-photo.component';
+import { SmallPhotoComponent } from './components/small-photo/small-photo.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    BigPhotoComponent,
+    SmallPhotoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/small-photo/small-photo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-photo',
+  templateUrl: './small-photo.component.html',
+  styleUrls: ['./small-photo.component.css']
+})
+export class SmallPhotoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
